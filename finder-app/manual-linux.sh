b1d715ca57e7a5560cf68940ad2f076f6b6fd7b3
PilotChalkanov <nik.chalkanov@gmail.com>
2025-03-16 14:32:01 +0200

    fix command

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -107,6 +107,6 @@ sudo chown -R root:root "${OUTDIR}/rootfs"
 
 # Create initramfs.cpio.gz
 cd "${OUTDIR}/rootfs"
-find .| cpio -H newc -ov --owner root:root > "${OUTDIR}/initramfs.cpio"
+find . | cpio -H newc -ov --owner root:root > "${OUTDIR}/initramfs.cpio"
 cd "${OUTDIR}" & ls
 gzip -f initramfs.cpio
