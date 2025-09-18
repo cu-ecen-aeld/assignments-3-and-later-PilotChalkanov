3e576c57378c4dbd58f9cba4ad5c4275fa94efa8
PilotChalkanov <nik.chalkanov@gmail.com>
2025-09-18 10:04:44 +0300

    memory alloc/freeing managed by the caller

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -46,8 +46,8 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
+
     if (buffer->full) {
-        free(buffer->entry[0]);
         buffer->entry[0] = *add_entry;
         buffer->in_offs = 0;
         buffer->out_offs = 1;
