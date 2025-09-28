c37b707f487c262aff73d6254955964265b57f5e
PilotChalkanov <nik.chalkanov@gmail.com>
2025-09-28 13:40:12 +0300

    add aesd_buffer_entry in aesd_dev

--- aesd-char-driver/aesdchar.h
+++ aesd-char-driver/aesdchar.h
@@ -29,8 +29,9 @@ struct aesd_dev
      * TODO: Add structure(s) and locks needed to complete assignment requirements
      */
     struct cdev cdev;
-    struct semaphore sem;
-    char *buffer;
+    struct mutex lock;
+    struct aesd_circular_buffer buffer;
+    struct aesd_buffer_entry entry;
 
 };
 
