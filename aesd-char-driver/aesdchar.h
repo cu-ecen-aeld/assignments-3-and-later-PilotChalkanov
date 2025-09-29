c3e5869b088a8c82360c887123544c6af1d6fec6
PilotChalkanov <nik.chalkanov@gmail.com>
2025-09-29 19:10:18 +0300

    change type semaphore -> mutex

--- aesd-char-driver/aesdchar.h
+++ aesd-char-driver/aesdchar.h
@@ -29,7 +29,7 @@ struct aesd_dev
      * TODO: Add structure(s) and locks needed to complete assignment requirements
      */
     struct cdev cdev;
-    struct semaphore lock;
+    struct mutex lock;
     struct aesd_circular_buffer buffer;
     struct aesd_buffer_entry entry;
 
