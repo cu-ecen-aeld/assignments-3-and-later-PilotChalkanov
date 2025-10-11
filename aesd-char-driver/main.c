e1f722154e446a5f44df948dc50ee465eee8cfad
PilotChalkanov <nik.chalkanov@gmail.com>
2025-10-11 16:11:31 +0300

    fix pointer to buffer

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -214,21 +214,21 @@ long aesd_ioctl(struct file *filp, unsigned int cmd, unsigned long arg){
         return -ERESTARTSYS;
 
     seekto = kmalloc(sizeof(struct aesd_seekto), GFP_KERNEL);
-    struct aesd_circular_buffer *buffer = dev->buffer;
+
     switch (cmd) {
         case AESDCHAR_IOCSEEKTO:
             if (copy_from_user(&seekto, (const void __user *)arg, sizeof(seekto))) {
                 ret = -EFAULT;
                 break;
             }
-            uint8_t num_entries = aesd_circular_buffer_get_full_count(buffer);
+            uint8_t num_entries = aesd_circular_buffer_get_full_count(&dev->buffer);
             if (seekto->write_cmd >= num_entries) {
                 ret = -EINVAL;
                 break;
             }
 
-            uint8_t cmd_idx = (buffer->out_offs + seekto->write_cmd) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
-            struct aesd_buffer_entry *entry = &buffer->entry[cmd_idx];
+            uint8_t cmd_idx = (&dev->buffer->out_offs + seekto->write_cmd) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+            struct aesd_buffer_entry *entry = &dev->buffer->entry[cmd_idx];
 
             if (seekto->write_cmd_offset >= entry->size) {
                 ret = -EINVAL;
@@ -238,8 +238,8 @@ long aesd_ioctl(struct file *filp, unsigned int cmd, unsigned long arg){
             // Calculate the file position
             loff_t new_f_pos = 0;
             for (uint8_t i = 0; i < seekto->write_cmd; i++) {
-                uint8_t idx = (buffer->out_offs + i) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
-                new_f_pos += buffer->entry[idx].size;
+                uint8_t idx = (&dev->buffer->out_offs + i) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+                new_f_pos += &dev->buffer->entry[idx].size;
             }
             new_f_pos += seekto->write_cmd_offset;
             filp->f_pos = new_f_pos;
