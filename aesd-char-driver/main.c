931a94d956b44c03644dc3285e57fcb6e013e1a2
PilotChalkanov <nik.chalkanov@gmail.com>
2025-10-11 14:42:30 +0300

    aesd_char -> unlocked_ioctl implement, add mutex locks for llseek and reformat code

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -24,16 +24,16 @@
 #include "aesd-circular-buffer.h"
 #include "aesdchar.h"
 
-int aesd_major =   0; // use dynamic major
-int aesd_minor =   0;
+int aesd_major = 0; // use dynamic major
+int aesd_minor = 0;
 
 MODULE_AUTHOR("Nikolay Chalkanov");
+
 MODULE_LICENSE("Dual BSD/GPL");
 
 struct aesd_dev aesd_device;
 
-int aesd_open(struct inode *inode, struct file *filp)
-{
+int aesd_open(struct inode *inode, struct file *filp) {
     PDEBUG("open");
     printk(KERN_INFO "aesd_open\n");
 
@@ -51,57 +51,54 @@ int aesd_open(struct inode *inode, struct file *filp)
     PDEBUG("Device opened successfully");
     printk(KERN_INFO "Device opened successfully\n");
     return 0;
-     /* success */
+    /* success */
 }
 
-int aesd_release(struct inode *inode, struct file *filp)
-{
+int aesd_release(struct inode *inode, struct file *filp) {
     PDEBUG("release");
 
     return 0;
 }
 
 ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
-                loff_t *f_pos)
-{
+                  loff_t *f_pos) {
     ssize_t retval = 0;
-    PDEBUG("read %zu bytes with offset %lld",count,*f_pos);
+    PDEBUG("read %zu bytes with offset %lld", count, *f_pos);
     struct aesd_dev *dev = filp->private_data;
     if (!dev) {
         return -ENODEV;
     }
 
-    if(mutex_lock_interruptible(&dev->lock))
+    if (mutex_lock_interruptible(&dev->lock))
         return -ERESTARTSYS;
 
     size_t entry_offset;
     struct aesd_buffer_entry *entry = aesd_circular_buffer_find_entry_offset_for_fpos(&dev->buffer,
         *f_pos, &entry_offset);
-    if(!entry){
+    if (!entry) {
         PDEBUG("No data available");
         printk("No data available");
         goto out;
-        }
-     size_t bytes_available = entry->size - entry_offset;
-     PDEBUG("bytes_available %zu",bytes_available);
-     size_t bytes_to_read = (count < bytes_available) ? count : bytes_available;
+    }
+    size_t bytes_available = entry->size - entry_offset;
+    PDEBUG("bytes_available %zu", bytes_available);
+    size_t bytes_to_read = (count < bytes_available) ? count : bytes_available;
 
-     if (copy_to_user(buf, entry->buffptr + entry_offset, bytes_to_read)) {
-         retval = -EFAULT;
-         goto out;
-     }
-     *f_pos += bytes_to_read;
-     retval = bytes_to_read;
-     PDEBUG("read %zu bytes with offset %lld",bytes_to_read,*f_pos);
+    if (copy_to_user(buf, entry->buffptr + entry_offset, bytes_to_read)) {
+        retval = -EFAULT;
+        goto out;
+    }
+    *f_pos += bytes_to_read;
+    retval = bytes_to_read;
+    PDEBUG("read %zu bytes with offset %lld", bytes_to_read, *f_pos);
 
-    out:
-        mutex_unlock(&dev->lock);
-        return retval;
+out:
+    mutex_unlock(&dev->lock);
+    return retval;
 }
 
 ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
-                   loff_t *f_pos)
-{
+                   loff_t *f_pos) {
     ssize_t retval = -ENOMEM;
     PDEBUG("write %zu bytes with offset %lld", count, *f_pos);
     printk(KERN_INFO "write %zu bytes with offset %lld", count, *f_pos);
@@ -169,48 +166,108 @@ out:
     return retval;
 }
 
+
 loff_t aesd_llseek(struct file *filp, loff_t offset, int whence) {
     struct aesd_dev *dev = filp->private_data;
-    loff_t new_pos;
-    switch(whence) {
-        case 0: /* SEEK_SET */
+    loff_t new_pos = 0;
+    loff_t buffer_size = 0;
+
+    if (mutex_lock_interruptible(&dev->lock))
+        return -ERESTARTSYS;
+    for (uint8_t i = 0; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; i++) {
+        if (dev->buffer.entry[i].buffptr)
+            buffer_size += dev->buffer.entry[i].size;
+    }
+    switch (whence) {
+        case SEEK_SET:
             new_pos = offset;
             break;
-
-        case 1: /* SEEK_CUR */
+        case SEEK_CUR:
             new_pos = filp->f_pos + offset;
             break;
-
-        case 2: /* SEEK_END */
-            new_pos = dev->size + offset;
+        case SEEK_END:
+            new_pos = buffer_size + offset;
             break;
-
-        default: /* can't happen */
+        default:
+            mutex_unlock(&dev->lock);
             return -EINVAL;
     }
-    if (new_pos < 0) return -EINVAL;
+
+    if (new_pos < 0 || new_pos > buffer_size) {
+        mutex_unlock(&dev->lock);
+        return -EINVAL;
+    }
+
     filp->f_pos = new_pos;
+    mutex_unlock(&dev->lock);
     return new_pos;
 }
 
 
+long aesd_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)f(mutex_lock_interruptible(&dev->lock)) {
+
+    struct aesd_dev *dev = filp->private_data;
+    struct aesd_seekto seekto;
+    long ret = 0;
+
+    if (mutex_lock_interruptible(&dev->lock))
+        return -ERESTARTSYS;
+
+    switch (cmd) {
+        case AESDCHAR_IOCSEEKTO:
+            if (copy_from_user(&seekto, (const void __user *)arg, sizeof(seekto))) {
+                ret = -EFAULT;
+                break;
+            }
+            uint8_t num_entries = aesd_circular_buffer_get_full_count(buffer);
+            if (seekto.write_cmd >= num_entries) {
+                ret = -EINVAL;
+                break;
+            }
+
+            uint8_t cmd_idx = (buffer->out_offs + seekto.write_cmd) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+            struct aesd_buffer_entry *entry = &buffer->entry[cmd_idx];
+
+            if (seekto.write_cmd_offset >= entry->size) {
+                ret = -EINVAL;
+                break;
+            }
+
+            // Calculate the file position
+            loff_t new_f_pos = 0;
+            for (uint8_t i = 0; i < seekto.write_cmd; i++) {
+                uint8_t idx = (buffer->out_offs + i) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+                new_f_pos += buffer->entry[idx].size;
+            }
+            new_f_pos += seekto.write_cmd_offset;
+            filp->f_pos = new_f_pos;
+            break;
+        default:
+            ret = -ENOTTY;
+            break;
+    }
+    mutex_unlock(&dev->lock);
+    return ret;
+}
+
+
 struct file_operations aesd_fops = {
-    .owner =    THIS_MODULE,
-    .read =     aesd_read,
-    .write =    aesd_write,
-    .open =     aesd_open,
-    .llseek =   aesd_llseek,
-    .release =  aesd_release,
+    .owner = THIS_MODULE,
+    .read = aesd_read,
+    .write = aesd_write,
+    .open = aesd_open,
+    .llseek = aesd_llseek,
+    .release = aesd_release,
+    .unlocked_ioctl = aesd_ioctl,
 };
 
-static int aesd_setup_cdev(struct aesd_dev *dev)
-{
+static int aesd_setup_cdev(struct aesd_dev *dev) {
     int err, devno = MKDEV(aesd_major, aesd_minor);
 
     cdev_init(&dev->cdev, &aesd_fops);
     dev->cdev.owner = THIS_MODULE;
     dev->cdev.ops = &aesd_fops;
-    err = cdev_add (&dev->cdev, devno, 1);
+    err = cdev_add(&dev->cdev, devno, 1);
     if (err) {
         printk(KERN_ERR "Error %d adding aesd cdev", err);
     }
@@ -218,18 +275,17 @@ static int aesd_setup_cdev(struct aesd_dev *dev)
 }
 
 
-int aesd_init_module(void)
-{
+int aesd_init_module(void) {
     dev_t dev = 0;
     int result;
     result = alloc_chrdev_region(&dev, aesd_minor, 1,
-            "aesdchar");
+                                 "aesdchar");
     aesd_major = MAJOR(dev);
     if (result < 0) {
         printk(KERN_WARNING "Can't get major %d\n", aesd_major);
         return result;
     }
-    memset(&aesd_device,0,sizeof(struct aesd_dev));
+    memset(&aesd_device, 0, sizeof(struct aesd_dev));
 
     /**
      * TODO: initialize the AESD specific portion of the device
@@ -241,15 +297,13 @@ int aesd_init_module(void)
 
     result = aesd_setup_cdev(&aesd_device);
 
-    if( result ) {
+    if (result) {
         unregister_chrdev_region(dev, 1);
     }
     return result;
-
 }
 
-void aesd_cleanup_module(void)
-{
+void aesd_cleanup_module(void) {
     dev_t devno = MKDEV(aesd_major, aesd_minor);
 
     cdev_del(&aesd_device.cdev);
@@ -274,4 +328,5 @@ void aesd_cleanup_module(void)
 }
 
 module_init(aesd_init_module);
+
 module_exit(aesd_cleanup_module);
